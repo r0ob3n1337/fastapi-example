3de799b91a371ae5ef6d7137224f9ac30b5ce0fe
r0ob3n1337 <r00b3n1337@gmail.com>
2024-03-02 17:54:25 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,25 @@
+"""Main module for test fastapi"""
+
+from fastapi import FastAPI
+from fastapi.responses import FileResponse, JSONResponse
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class SNums(BaseModel):
+    num1: int
+    num2: int
+
+
+@app.get("/")
+async def root():
+    """Index root page"""
+    return FileResponse("./index.html")
+
+
+@app.post("/calculate")
+async def calculate(body: SNums):
+    """Endpoint for sum two numbers"""
+    result = body.num1 + body.num2
+    return JSONResponse({"result": result})
